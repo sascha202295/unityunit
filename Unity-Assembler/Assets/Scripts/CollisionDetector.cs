c184e21a0d9a8ea6aae102d59d6ecf58c88809d2
Nasser <greatman502@gmail.com>
2020-11-10 13:31:39 +0100

    Add collision and overlap detector

---
+++ Unity-Assembler/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col)
+    {
+       // Debug.Log( col.gameObject.name);
+
+        Debug.Log("collision!!");
+    }
+
+    // OnTriggerEnter is called to detect overlap
+    private void OnTriggerEnter(Collider other)
+    {  
+        Debug.Log("trrigger!!");
+    }
+}
