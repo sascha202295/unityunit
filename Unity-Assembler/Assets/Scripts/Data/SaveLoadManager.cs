7e4435e7682c1594c012c53379463df2b95a5da8
Nasser <greatman502@gmail.com>
2021-01-13 00:43:07 +0100

    update save methode "json"

--- Unity-Assembler/Assets/Scripts/Data/SaveLoadManager.cs
+++ Unity-Assembler/Assets/Scripts/Data/SaveLoadManager.cs
@@ -1,12 +1,60 @@
 ﻿using UnityEngine;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
+using System.Xml;
 using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+using System.Text;
+using System;
+using System.Runtime.Serialization.Json;
+
+using System.Linq;
+using Valve.Newtonsoft.Json;
 
 public class SaveLoadManager : MonoBehaviour
 {
     /// Handle saving data to File
+    /// 
+    private void Start()
+    {
 
+
+        
+        List<Part> parts = new List<Part>();
+        parts.Add(new Part("test1"));
+        List<Station> stations = null;
+        Station station = new Station(parts, stations);
+        station.Position = new Vector3(1, 2, 3);
+        station.Rotation = new Quaternion();
+
+
+        string output = station.stationSerializer();
+
+        SaveToFile(stationArraysJson(new String[] { output, output }), "test");
+
+
+        Debug.Log(output);
+
+
+    }
+    private void Update()
+    {
+
+    }
+    public static void SaveToFile(String json, String fileName)
+    {
+        // Set the path to the persistent data path (works on most devices by default)
+        string path = Application.persistentDataPath + "/saves/";
+        Debug.Log("Path: " + path);
+        // Create the directory IF it doesn't already exist
+        Directory.CreateDirectory(path);
+        File.WriteAllText(@path + fileName + ".json", json);
+
+    }
+    /*
     public static void SaveToFile<T>(T data, string fileName)
     {
         // Set the path to the persistent data path (works on most devices by default)
@@ -39,31 +87,40 @@ public class SaveLoadManager : MonoBehaviour
             fileStream.Close();
         }
     }
-
+    */
     /// Load data using a string fileName
     public static T Load<T>(string fileName)
     {
         // Set the path to the persistent data path (works on most devices by default)
         string path = Application.persistentDataPath + "/saves/";
+
+        JsonSerializer serializer = new JsonSerializer();
+
         // Grab an instance of the BinaryFormatter that will handle serializing our data
         // BinaryFormatter formatter = new BinaryFormatter();
 
         // Using xml serializer
-        var serializer = new XmlSerializer(typeof(T));
+        // var serializer = new XmlSerializer(typeof(T));
 
         // Open up a filestream, combining the path and fileName
-        FileStream fileStream = new FileStream(path + fileName + ".xml", FileMode.Open);
+        FileStream fileStream = new FileStream(path + fileName + ".json", FileMode.Open);
         // Initialize a variable with the default value of whatever type we're using
-        T returnData = default(T);
+        //T returnData = default(T);
+
         /* 
          * Try/Catch/Finally block that will attempt to deserialize the data
          * If we fail to successfully deserialize the data, we'll just return the default value for Type
          */
+        // T obj = null;
         try
         {
             // returnValue = (T)formatter.Deserialize(fileStream);
 
-            returnData = (T)serializer.Deserialize(fileStream);
+            //returnData = (T)serializer.Deserialize(fileStream);
+            var sr = new StreamReader(fileStream);
+            string contents = sr.ReadToEnd();
+            T obj = JsonConvert.DeserializeObject<T>(contents);
+            return obj;
         }
         catch (SerializationException exception)
         {
@@ -73,12 +130,37 @@ public class SaveLoadManager : MonoBehaviour
         {
             fileStream.Close();
         }
+
+
+        /*
         foreach (GameObject g in returnData as GameObject[])
         {
             Instantiate(g);
-        }
-        return returnData;
+        }*/
+
+        return default(T);
     }
 
+
+    public String stationArraysJson(String[] stationArray)
+    {
+        String json = "\"stations\": [\n ";
+        foreach (var stationJson in stationArray)
+        {
+            json += "\n" + stationJson + ",";
+
+        }
+        json += "]";
+        return json;
+    }
+
+
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
+    }
 
 }
--- Unity-Assembler/Assets/Scripts/Data/Station.cs
+++ Unity-Assembler/Assets/Scripts/Data/Station.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.Newtonsoft.Json;
 
 public class Station
 {
@@ -62,4 +63,40 @@ public class Station
         }
         return tmpParts;
     }
+
+
+
+    public struct StationData
+    {
+        public string Name { get; set; }
+        public List<Part> PartList { get; set; }
+        public List<Station> PreviousStations { get; set; }
+        public float[] Position;
+        public float[] Rotation;
+
+    }
+
+    public static Station stationDeserializer(string json)
+    {
+
+        StationData stationData = JsonConvert.DeserializeObject<StationData>(json);
+        Station station = new Station(stationData.PartList, stationData.PreviousStations);
+        station.Position = new Vector3(stationData.Position[0], stationData.Position[1], stationData.Position[2]);
+        station.Rotation = new Quaternion(stationData.Position[0], stationData.Position[1], stationData.Position[2], stationData.Position[3]);
+        return station;
+    }
+    public string stationSerializer()
+    {
+        StationData stationData = new StationData
+        {
+            Name = this.Name,
+            PartList = this.PartList,
+            PreviousStations = this.PreviousStations,
+            Position = new float[3] { this.Position.x, this.Position.y, this.Position.z },
+            Rotation = new float[4] { this.Rotation.x, this.Rotation.y, this.Rotation.z, this.Rotation.w }
+        };
+
+        return JsonConvert.SerializeObject(stationData, Formatting.Indented);
+    }
+
 }
