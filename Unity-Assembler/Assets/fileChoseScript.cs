916e2acbca7b2921c73d6f0fd85cc1207047b711
sascha202295 <72446840+sascha202295@users.noreply.github.com>
2021-01-15 16:39:08 +0100

    file choser script

--- Unity-Assembler/Assets/fileChoseScript.cs
+++ Unity-Assembler/Assets/fileChoseScript.cs
@@ -2,120 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using System.IO;
 
 public class fileChoseScript : MonoBehaviour
 {
     public GameObject mListItemSpawnpoint;
     public GameObject mListItem;
     public GameObject mUIPointer;
-    public GameObject stationModel;
-    public Station station;
+
     public Material mItemSpawnMaterial;
     public Material mItemBuildMaterial;
     public Vector3 mItemSpawnScale;
 
     private Vector3 mItemSpawnpoint = new Vector3(4.0f, 0.3f, -3.0f);
     private Vector3 mItemSpawnRotation = new Vector3(0, -90.0f, 0);
-
-    private GameObject partHolder;
-
-    private List<Part> partList;
+    private List<string> names;
+   
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponentInChildren<Canvas>().worldCamera = mUIPointer.GetComponent<Camera>();
-        partList = new List<Part>(station.PartList);
-        UpdateScrollView();
 
-        // create parent object for parts, to make deletion of spawned parts easier
-        partHolder = new GameObject();
-        partHolder.name = "partHolder";
-        partHolder.transform.parent = transform.parent;
-        partHolder.transform.position = transform.root.position;
-    }
+        List<string> files  = new List<string>();
+        string path = Application.persistentDataPath + "/saves/";
 
-    private void UpdateScrollView()
-    {
-        // delete preexisting items
-        foreach (Transform listItem in mListItemSpawnpoint.transform)
-        {
-            Destroy(listItem.gameObject);
+        DirectoryInfo d = new DirectoryInfo(@"path");
+
+
+        string[] filenames = Directory.GetFiles(path);
+
+        // saving file names in path in string list
+        foreach (var fi in d.GetFiles())
+        {    
+            // remove json tag from file name
+            string name = fi.Name;
+            string name1 = name.Remove(name.Length - 6, 5);
+            files.Add(name1);
+
         }
-        // populate list if there are any
-        if (partList != null)
+
+        if (names != null)
         {
-            foreach (Part part in partList)
+            foreach (string s in names)
             {
-                // do not create Listitems for already placed parts
-                if (part.IsPlaced)
-                {
-                    continue;
-                }
+               
                 //instantiate item and set parent
                 GameObject spawnedItem = Instantiate(mListItem, mListItemSpawnpoint.transform, false);
-                spawnedItem.GetComponentInChildren<Text>().text = part.Name;
+                spawnedItem.GetComponentInChildren<Text>().text = s;
                 spawnedItem.GetComponent<Button>().onClick.AddListener(() => this.ItemClicked(spawnedItem));
             }
+
         }
+
+
+
+
+    
+
+    
     }
 
     public void ItemClicked(GameObject button)
     {
         string tmp = button.GetComponentInChildren<Text>().text;
 
-        foreach (Part part in partList)
-        {
-            if (part.Name.Equals(tmp))
-            {
-                //spawn and position part
-                GameObject tmpPart = Instantiate(stationModel.transform.GetChild(part.PartID).gameObject, partHolder.transform);
-                tmpPart.transform.localScale = Vector3.Scale(tmpPart.transform.localScale, mItemSpawnScale);
-                tmpPart.transform.localPosition = mItemSpawnpoint;
-                // Apply rotation
-                Quaternion tmpQuat = tmpPart.transform.localRotation;
-                tmpQuat.eulerAngles = mItemSpawnRotation;
-                tmpPart.transform.localRotation = tmpQuat;
-                //add rigidbody and set meshcollider variables
-                Rigidbody tmpRig = tmpPart.AddComponent<Rigidbody>();
-                tmpRig.useGravity = true;
-                tmpRig.isKinematic = true;
-                foreach (MeshCollider col in tmpPart.GetComponents<MeshCollider>())
-                {
-                    col.isTrigger = false;
-                }
-                // extra capsule collider needed as meshcolliders of smaller parts are unreliable
-                tmpPart.AddComponent<CapsuleCollider>().isTrigger = true;
-                // set material
-                Utils.SetObjectMaterial(tmpPart.transform, mItemSpawnMaterial);
-                // destroy ProductAssemblyCollider, as it is not needed
-                Destroy(tmpPart.GetComponent<ProductAssemblyCollider>());
+        PlayerPrefs.SetString("fileName", "s");
 
-                partList.Remove(part);
-
-                UpdateScrollView();
-                return;
-            }
-        }
     }
 
-    public void PartPlaced(Part part)
-    {
-        station.PartPlaced(part);
-    }
 
-    public void ResetStation()
-    {
-        partList = new List<Part>(station.PartList);
-        foreach (Part part in partList)
-        {
-            Utils.SetObjectMaterial(stationModel.transform.GetChild(part.PartID), mItemBuildMaterial);
-            part.IsPlaced = false;
-        }
-        UpdateScrollView();
-        foreach (Transform part in partHolder.transform)
-        {
-            Destroy(part.gameObject);
-        }
-    }
+    
 }
