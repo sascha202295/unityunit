7765f70fa8ac0b7493e6877665a14158f2b2bb7c
Jan Krebes <jankrebes@gmail.com>
2021-01-15 17:01:34 +0100

    grobe Fehler in FileChooseScript behoben

--- Unity-Assembler/Assets/Scripts/MainMenu.cs
+++ Unity-Assembler/Assets/Scripts/MainMenu.cs
@@ -6,11 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    //material and scale for filechoser screen
-    /* public Material materialTransparent;
-      public Material materialOpaque;
-      private Vector3 scale = new Vector3(1.5f, 1.5f, 1.5f);
-    */
+    public GameObject mUIPointer;
     public Text statusText;
 
     void Awake()
@@ -29,17 +25,12 @@ public class MainMenu : MonoBehaviour
         statusText.text = "Loading Project...";
         PlayerPrefs.SetString("fileName", "test");
         SceneManager.LoadScene("Game");
-
-
         /*
         //place filechoser screen
         GameObject choserScreen = GameObject.Instantiate((GameObject)Resources.Load("FileChoser"), this.transform);
-        choserScreen.transform.localPosition = this.transform;
-        fileChosescript tmp = choserScreen.GetComponent<fileChosescript>();
-        tmp.mUIPointer = ui_Pointer;
-        tmp.mItemSpawnScale = scale;
-        tmp.mItemSpawnMaterial = materialOpaque;
-        tmp.mItemBuildMaterial = materialTransparent;
+        choserScreen.transform.localPosition = transform.position;
+        FileChooseScript tmp = choserScreen.GetComponent<FileChooseScript>();
+        tmp.mUIPointer = mUIPointer;
         */
     }
 
